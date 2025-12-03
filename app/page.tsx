2dca43b20e20a3137d5a443e19765ab206613fbc
Your Name <wickramasooriyabgpv.22@uom.lk>
2025-12-03 18:48:49 +0530

    feat: add global styles and update homepage layout

--- app/page.tsx
+++ app/page.tsx
@@ -7,12 +7,12 @@ import type { UIMessage } from "ai";
 export default function Home() {
     const { input, handleInputChange, handleSubmit, append, isLoading, messages } = useChat()
 
-    const onMessage = false
+    const noMessage = true
   return (
     <main>
-      <Image src={logoo} alt="Logo" width={275} height={50} />
-      <section>
-        {onMessage ? (
+      <Image src={logoo} alt="Logo" width={300} height={50} />
+      <section className={noMessage ? "" : "populated"}>
+        {noMessage ? (
             <>
             <p className="starter-text">
                 Welcome to Ceylon Chronicles! Ask me anything about the rich history of Sri Lanka. You can explore historical insights from the prehistoric era to modern times 
@@ -25,11 +25,11 @@ export default function Home() {
             {/* <LoadingBubbles/> */}
             </>
         )}
-        <form onSubmit={handleSubmit}>
+      </section>
+      <form onSubmit={handleSubmit}>
             <input className="question-box" onChange={handleInputChange} value={input} type="text" placeholder="Ask me some insights..." />
             <input type="submit" />
         </form>
-      </section>
     </main>
   );
 }
