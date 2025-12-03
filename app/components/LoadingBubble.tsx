4710e1a2f2117eb085ab3df4c1031cbea0f4a950
Your Name <wickramasooriyabgpv.22@uom.lk>
2025-12-03 18:51:56 +0530

    new files added

---
+++ app/components/LoadingBubble.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const LoadingBubble = () => {
+  return (
+    <>
+    </>
+  )
+}
+
+export default LoadingBubble
---
+++ app/components/PromptSuggestionRow.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PromptSuggestionRow = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default PromptSuggestionRow
